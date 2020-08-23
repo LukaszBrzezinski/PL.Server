d5181a816146ad120e27c59adc3894d558c15991
LukaszBrzezinski <lukasz.grzegorz.brzezinski@gmail.com>
2020-08-23 21:42:37 +0200

    add client to identity server configuration

--- src/Authorization/PL.Authorization/Configurations/IdentityServerConfig.cs
+++ src/Authorization/PL.Authorization/Configurations/IdentityServerConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using IdentityModel;
+using IdentityServer4;
 using IdentityServer4.Models;
 
 namespace PL.Authorization.Configurations
@@ -10,7 +11,7 @@ namespace PL.Authorization.Configurations
     {
         public const string ApiName = "pl_api";
         public const string ApiFriendlyName = "Private Lessons API";
-        public const string QuickAppClientID = "pl_app_spa";
+        public const string SpaClientID = "pl_app_spa";
         public const string SwaggerClientID = "swagger_ui";
 
         // Identity resources (used by UserInfo endpoint).
@@ -38,6 +39,52 @@ namespace PL.Authorization.Configurations
                         JwtClaimTypes.PhoneNumber,
                         JwtClaimTypes.Role,
                         ClaimConstants.Permission
+                    }
+                }
+            };
+        }
+
+        // Clients want to access resources.
+        public static IEnumerable<Client> GetClients()
+        {
+            // Clients credentials.
+            return new List<Client>
+            {
+                // http://docs.identityserver.io/en/release/reference/client.html.
+                new Client
+                {
+                    ClientName = "SPA application",
+                    ClientId = SpaClientID,
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword, // Resource Owner Password Credential grant.
+                    AllowAccessTokensViaBrowser = true,
+                    RequireClientSecret = false, // This client does not need a secret to request tokens from the token endpoint.
+                    
+                    AllowedScopes = {
+                        IdentityServerConstants.StandardScopes.OpenId, // For UserInfo endpoint.
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Phone,
+                        IdentityServerConstants.StandardScopes.Email,
+                        ScopeConstants.Roles,
+                        ApiName
+                    },
+                    AllowOfflineAccess = true, // For refresh token.
+                    RefreshTokenExpiration = TokenExpiration.Sliding,
+                    RefreshTokenUsage = TokenUsage.OneTimeOnly,
+                    //AccessTokenLifetime = 900, // Lifetime of access token in seconds.
+                    //AbsoluteRefreshTokenLifetime = 7200,
+                    //SlidingRefreshTokenLifetime = 900,
+                },
+
+                new Client
+                {
+                    ClientId = SwaggerClientID,
+                    ClientName = "Swagger UI",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    AllowAccessTokensViaBrowser = true,
+                    RequireClientSecret = false,
+
+                    AllowedScopes = {
+                        ApiName
                     }
                 }
             };
