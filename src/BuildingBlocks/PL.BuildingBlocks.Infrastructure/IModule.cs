20af22e455e4d70abbe8bbe2ed13df39b64cbd8d
LukaszBrzezinski <lukasz.grzegorz.brzezinski@gmail.com>
2020-08-23 21:51:16 +0200

    add basic interface for module

--- src/BuildingBlocks/PL.BuildingBlocks.Infrastructure/IModule.cs
+++ src/BuildingBlocks/PL.BuildingBlocks.Infrastructure/IModule.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace PL.BuildingBlocks.Infrastructure
 {
     public interface IModule
     {
+        void Setup(IServiceCollection services);
+        void Register(IServiceCollection services);
     }
 }
