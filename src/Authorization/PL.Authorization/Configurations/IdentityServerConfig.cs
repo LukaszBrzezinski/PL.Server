c569777cf60aaf9a4cda0c4e613d554bff9dd0ec
LukaszBrzezinski <lukasz.grzegorz.brzezinski@gmail.com>
2020-08-23 21:39:06 +0200

    add configuration for identity server

---
+++ src/Authorization/PL.Authorization/Configurations/IdentityServerConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IdentityModel;
+using IdentityServer4.Models;
+
+namespace PL.Authorization.Configurations
+{
+    public class IdentityServerConfig
+    {
+        public const string ApiName = "pl_api";
+        public const string ApiFriendlyName = "Private Lessons API";
+        public const string QuickAppClientID = "pl_app_spa";
+        public const string SwaggerClientID = "swagger_ui";
+
+        // Identity resources (used by UserInfo endpoint).
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Phone(),
+                new IdentityResources.Email(),
+                new IdentityResource(ScopeConstants.Roles, new List<string> { JwtClaimTypes.Role })
+            };
+        }
+
+        // Api resources.
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource(ApiName) {
+                    UserClaims = {
+                        JwtClaimTypes.Name,
+                        JwtClaimTypes.Email,
+                        JwtClaimTypes.PhoneNumber,
+                        JwtClaimTypes.Role,
+                        ClaimConstants.Permission
+                    }
+                }
+            };
+        }
+    }
+}
