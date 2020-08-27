5b9d4d29a81116127dba3a49176e150d88764f0a
LukaszBrzezinski <lukasz.grzegorz.brzezinski@gmail.com>
2020-08-27 17:30:37 +0200

    add abstraction on mediatr command handler

---
+++ PL.BuildingBlocks.Application/ICommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace PL.BuildingBlocks.Application
+{
+    public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand> where TCommand : ICommand
+    {
+    }
+}
