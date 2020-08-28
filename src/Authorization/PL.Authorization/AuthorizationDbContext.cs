cbe5249f9467f909971fc0ccbe347e881923913c
LukaszBrzezinski <lukasz.grzegorz.brzezinski@gmail.com>
2020-08-28 21:38:54 +0200

    clean authorization dbcontext

--- src/Authorization/PL.Authorization/AuthorizationDbContext.cs
+++ src/Authorization/PL.Authorization/AuthorizationDbContext.cs
@@ -1,32 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using PL.Authorization.Models;
+using PL.Authorization.Application.User;
 
 namespace PL.API.Authorization
 {
-    public class AuthorizationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    public class AuthorizationDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
         public AuthorizationDbContext(DbContextOptions options) : base(options)
         {
         }
-
-        //TODO: move to entity configurations:
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
-            // user
-            builder.Entity<ApplicationUser>().HasMany(u => u.Claims).WithOne().HasForeignKey(c => c.UserId).IsRequired().OnDelete(DeleteBehavior.Cascade);
-            builder.Entity<ApplicationUser>().HasMany(u => u.Roles).WithOne().HasForeignKey(r => r.UserId).IsRequired().OnDelete(DeleteBehavior.Cascade);
-
-            // role
-            builder.Entity<ApplicationRole>().HasMany(r => r.Claims).WithOne().HasForeignKey(c => c.RoleId).IsRequired().OnDelete(DeleteBehavior.Cascade);
-            builder.Entity<ApplicationRole>().HasMany(r => r.Users).WithOne().HasForeignKey(r => r.RoleId).IsRequired().OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
