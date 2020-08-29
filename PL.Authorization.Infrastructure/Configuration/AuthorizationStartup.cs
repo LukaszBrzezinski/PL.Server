5c3355ac656c6ff995fdf35d93c5a6eb02045493
LukaszBrzezinski <lukasz.grzegorz.brzezinski@gmail.com>
2020-08-29 12:09:41 +0200

    initial authorization module startup

---
+++ PL.Authorization.Infrastructure/Configuration/AuthorizationStartup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace PL.Authorization.Infrastructure.Configuration
+{
+    public class AuthorizationStartup
+    {
+
+        public static void Initialize(IConfiguration configuration, ILogger logger)
+        {
+
+        }
+
+        private static void ConfigureCompositionRoot()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+        }
+    }
+}
