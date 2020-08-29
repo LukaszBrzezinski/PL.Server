8111945c3f8d20091d71184dd10100fab973e295
LukaszBrzezinski <lukasz.grzegorz.brzezinski@gmail.com>
2020-08-29 12:48:54 +0200

    prepare for autofac DI

---
+++ PL.Authorization.Infrastructure/Configuration/Pipeline/ProcessingModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace PL.Authorization.Infrastructure.Configuration.Pipeline
+{
+    internal class ProcessingModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.
+        }
+    }
+}
