f967c417145a423b68dfde2595d7f499caa7f85a
LukaszBrzezinski <lukasz.grzegorz.brzezinski@gmail.com>
2020-08-29 21:39:40 +0200

    add unit of work

---
+++ src/BuildingBlocks/PL.BuildingBlocks.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PL.BuildingBlocks.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _dbContext;
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> CommitAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
