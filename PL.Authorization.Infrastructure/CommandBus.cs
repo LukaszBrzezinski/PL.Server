8bc1f251e625fe8d522ce2291b724729a36379a1
LukaszBrzezinski <lukasz.grzegorz.brzezinski@gmail.com>
2020-08-29 13:12:25 +0200

    separate query bus to separate file

--- PL.Authorization.Infrastructure/CommandBus.cs
+++ PL.Authorization.Infrastructure/CommandBus.cs
@@ -21,18 +21,4 @@ namespace PL.Authorization.Infrastructure
             await _mediator.Publish(command);
         }
     }
-
-    internal class QueryBus : IQueryBus
-    {
-        private readonly IMediator _mediator;
-
-        public QueryBus(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-        public async Task<TResult> ExecuteQueryAsync<TResult>(IQuery<TResult> query)
-        {
-            return await _mediator.Send(query);
-        }
-    }
 }
---
+++ PL.Authorization.Infrastructure/QueryBus.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using PL.BuildingBlocks.Application;
+
+namespace PL.Authorization.Infrastructure
+{
+    internal class QueryBus : IQueryBus
+    {
+        private readonly IMediator _mediator;
+
+        public QueryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<TResult> ExecuteQueryAsync<TResult>(IQuery<TResult> query)
+        {
+            return await _mediator.Send(query);
+        }
+    }
+}
