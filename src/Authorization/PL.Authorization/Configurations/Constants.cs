65b4ed2523bd2f935a1745a1e2f05a9b0324e9dc
LukaszBrzezinski <lukasz.grzegorz.brzezinski@gmail.com>
2020-08-30 16:58:33 +0200

    move classes to valid folders

--- src/Authorization/PL.Authorization/Configurations/Constants.cs
+++ src/Authorization/PL.Authorization/Configurations/Constants.cs
@@ -14,13 +14,4 @@
         ///<summary>A scope that specifies the roles of an entity</summary>
         public const string Roles = "roles";
     }
-
-    public static class ModuleConstants
-    {
-        /// <summary>Entity framework migrations table name</summary>
-        public const string EF_MigrationsTable = "EF_Migrations";
-
-        /// <summary>Entity framework migrations table name</summary>
-        public const string DefaultSchema = "Authorization";
-    }
 }
