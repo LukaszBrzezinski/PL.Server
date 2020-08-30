4204dfd64e4dda31000b7143922e354163a417bb
LukaszBrzezinski <lukasz.grzegorz.brzezinski@gmail.com>
2020-08-30 11:56:13 +0200

    add authorization command handler

---
+++ PL.BuildingBlocks.Application/ISqlConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace PL.BuildingBlocks.Application
+{
+    public interface ISqlConnectionFactory
+    {
+        IDbConnection GetOpenConnection();
+    }
+}
--- src/Authorization/PL.Authorization/Authentication/AuthenticateCommandHandler.cs
+++ src/Authorization/PL.Authorization/Authentication/AuthenticateCommandHandler.cs
@@ -1,14 +1,56 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
 using System.Threading.Tasks;
+using Dapper;
+using PL.Authorization.Application.Configurations;
 using PL.BuildingBlocks.Application;
 
 namespace PL.Authorization.Application.Authentication
 {
     internal class AuthenticateCommandHandler : ICommandHandler<AuthenticateCommand, AuthenticationResult>
     {
-        public Task<AuthenticationResult> Handle(AuthenticateCommand request, CancellationToken cancellationToken)
+        private readonly ISqlConnectionFactory _connectionFactory;
+
+        public AuthenticateCommandHandler(ISqlConnectionFactory connectionFactory)
         {
-            throw new System.NotImplementedException();
+            _connectionFactory = connectionFactory;
+        }
+
+        public async Task<AuthenticationResult> Handle(AuthenticateCommand request, CancellationToken cancellationToken)
+        {
+            var connection = _connectionFactory.GetOpenConnection();
+
+            const string sql = "SELECT " +
+                               "[UserAccount].[Id], " +
+                               "[UserAccount].[Login], " +
+                               "[UserAccount].[Name], " +
+                               "[UserAccount].[Email], " +
+                               "[UserAccount].[IsActive], " +
+                               "[UserAccount].[Password] " +
+                               "FROM [auth].[UserAccounts] AS [UserAccount] " +
+                               "WHERE [User].[Login] = @Login";
+
+            var user = await connection.QuerySingleOrDefaultAsync<UserDto>(sql, new { request.Login });
+
+            if (user == null)
+            {
+                return new AuthenticationResult("Incorrect login or password");
+            }
+
+            if (!user.IsActive)
+            {
+                return new AuthenticationResult("User is not active");
+            }
+
+            if (!PasswordManager.VerifyHashedPassword(user.Password, request.Password))
+            {
+                return new AuthenticationResult("Incorrect login or password");
+            }
+
+            user.Claims = new List<Claim>();
+
+            return new AuthenticationResult(user);
         }
     }
 }
