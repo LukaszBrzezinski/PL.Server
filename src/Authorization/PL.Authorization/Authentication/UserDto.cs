af33c0a0263c1ddb4aa5d665fa2c0994081c6b98
LukaszBrzezinski <lukasz.grzegorz.brzezinski@gmail.com>
2020-08-30 13:58:01 +0200

    fix typo in user dto

--- src/Authorization/PL.Authorization/Authentication/UserDto.cs
+++ src/Authorization/PL.Authorization/Authentication/UserDto.cs
@@ -10,7 +10,7 @@ namespace PL.Authorization.Application.Authentication
         public string Login { get; set; }
         public bool IsActive { get; set; }
         public string FirstName { get; set; }
-        public string LasttName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public List<Claim> Claims { get; set; }
         public string Password { get; set; }
