abb886e2f58464b3ae8927a5e57f6634022ae9de
LukaszBrzezinski <lukasz.grzegorz.brzezinski@gmail.com>
2020-08-30 11:32:42 +0200

    add command handler with result interface

--- PL.BuildingBlocks.Application/ICommandHandler.cs
+++ PL.BuildingBlocks.Application/ICommandHandler.cs
@@ -8,4 +8,10 @@ namespace PL.BuildingBlocks.Application
     public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand> where TCommand : ICommand
     {
     }
+
+    public interface ICommandHandler<in TCommand, TResult> : IRequestHandler<TCommand, TResult>
+        where TCommand : ICommand<TResult>
+    {
+
+    }
 }
---
+++ src/Authorization/PL.Authorization/Authentication/AuthenticateCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using PL.BuildingBlocks.Application;
+
+namespace PL.Authorization.Application.Authentication
+{
+    internal class AuthenticateCommandHandler : ICommandHandler<AuthenticateCommand, AuthenticationResult>
+    {
+        public Task<AuthenticationResult> Handle(AuthenticateCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
